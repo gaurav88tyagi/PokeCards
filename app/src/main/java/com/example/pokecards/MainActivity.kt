57e20954c5067294ba3d1c1bec15b558f49c85d8
Gaurav Tyagi <gtyagi88cse@gmail.com>
2019-07-19 16:17:02 +0530

    Used LayoutManagerGroup library

--- app/src/main/java/com/example/pokecards/MainActivity.kt
+++ app/src/main/java/com/example/pokecards/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.pokecards
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.GridLayoutManager
+import com.dingmouren.layoutmanagergroup.echelon.EchelonLayoutManager
+import com.dingmouren.layoutmanagergroup.skidright.SkidRightLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
 
@@ -14,12 +16,16 @@ class MainActivity : AppCompatActivity() {
 
         val cards = genNRandomCards(100)
 
-        rvCards.layoutManager = GridLayoutManager(
-            this,
-            2,
-            GridLayoutManager.VERTICAL,
-            false
-        )
+        rvCards.layoutManager = EchelonLayoutManager(this)
+
+//        rvCards.layoutManager = SkidRightLayoutManager(1.0F, 1.2F)
+
+//        rvCards.layoutManager = GridLayoutManager(
+//            this,
+//            2,
+//            GridLayoutManager.VERTICAL,
+//            false
+//        )
 
         rvCards.adapter = CardAdapter(cards)
 
